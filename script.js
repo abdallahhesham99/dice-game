ceff037d96c7d661687a576826df65a405ba81ad
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-05-03 09:08:41 +0200

    switching the active player

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 //selecting elements
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,22 +12,41 @@ const rollButton = document.querySelector('.btn--roll');
 const newButton = document.querySelector('.btn--new');
 const holdButton = document.querySelector('.btn--hold');
 
+//default values
 let currentScore = 0;
+let activePlayer = 0;
 
+// =================================================================
 //Rolling dice function
 rollButton.addEventListener('click', function () {
-  //1. Generating a random dice
+  //1.Generating a random dice
   const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2. display dice
+  //2.display dice
   diceImg.classList.remove('d-none');
   diceImg.src = `imgs/dice-${dice}.png`;
-  //3. check if dice roll === 1
+  //check dice != 1
   if (dice !== 1) {
-    //add dice to current score
+    //add dice number to current score value
     currentScore += dice;
-    current0El.textContent = currentScore;
-  } else {
-    // Switch player
+
+    //add current score value to  player depends on activeplayer 0 or 1
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  }
+  //switch player
+  else {
+    //when dice === 1 reset activeplayer current score
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+    //reset current score value
+    currentScore = 0;
+
+    //if activePlayer ===0 make it === 1 and vice versa
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    //add and remove player--active class
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
